e5b4a9fc9ca00ecdd16dcee93c61b2d3e5f90a52
mkrft <kreft.manuel-it19@it.dhbw-ravensburg.de>
2022-04-30 13:42:48 +0200

    Implemented synchronization, needs some more testing to determine functionality of lock-mechanism

--- blackboard_server_main.py
+++ blackboard_server_main.py
@@ -13,10 +13,14 @@ from rpyc.utils.server import ThreadedServer
 
 import time
 import json
+from threading import Lock
 
 
 #=====Global == But may want to use JSON instead of global param!==
 boards = {}
+
+# Created lock, perhaps think of another solution than creating as "global" lock
+board_lock = Lock()
 
 #=====Server Class====================================
 class BlackBoardHost(rpyc.Service):
@@ -42,24 +46,25 @@ class BlackBoardHost(rpyc.Service):
         except ValueError():
             return (False, "Invalid Parameters! Please give Valid Time in Seconds as Float or Int")
 
-        # Check if name already given
-        if name not in boards:
+        with board_lock:
+            # Check if name already given
+            if name not in boards:
 
-            # Init the new blackboard
-            new_blackboard = {
-                "name" : name,
-                "valid_sec" : valid_sec,
-                "entry_time" : time.time(),
-                "is_valid" : True,
-                "data" : ""
-            }
+                # Init the new blackboard
+                new_blackboard = {
+                    "name" : name,
+                    "valid_sec" : valid_sec,
+                    "entry_time" : time.time(),
+                    "is_valid" : True,
+                    "data" : ""
+                }
 
-            # Add to boards
-            # TODO Maybe write to a JSON instead
-            boards[name] = new_blackboard
+                # Add to boards
+                # TODO Maybe write to a JSON instead
+                boards[name] = new_blackboard
 
-        else:
-            return (False, f"Board Name '{name}' already exists")
+            else:
+                return (False, f"Board Name '{name}' already exists")
 
         # TODO Delete for debug
         self.debug_print()
@@ -82,18 +87,19 @@ class BlackBoardHost(rpyc.Service):
 
         global boards
 
+        with board_lock:
         # Check if the blackboard exists
-        if name in boards:
+            if name in boards:
 
-            # Update the board information
-            # TODO Consistency
-            boards[name]["entry_time"] = time.time()
-            boards[name]["data"] = data
-            boards[name]["is_valid"] = True
+                # Update the board information
+                # TODO Consistency
+                boards[name]["entry_time"] = time.time()
+                boards[name]["data"] = data
+                boards[name]["is_valid"] = True
 
-        # If not return error
-        else:
-            return (False, "Board does not exist!")
+            # If not return error
+            else:
+                return (False, "Board does not exist!")
 
         # TODO delte
         self.debug_print()
@@ -115,17 +121,19 @@ class BlackBoardHost(rpyc.Service):
 
         global boards
 
-        # Check if the blackboard exists
-        if name in boards:
+        with board_lock:
 
-            # Update the board information
-            # TODO Consistency
-            boards[name]["data"] = ""
-            boards[name]["is_valid"] = False
+            # Check if the blackboard exists
+            if name in boards:
 
-        # If not return error
-        else:
-            return (False, "Board does not exist!")
+                # Update the board information
+                # TODO Consistency
+                boards[name]["data"] = ""
+                boards[name]["is_valid"] = False
+
+            # If not return error
+            else:
+                return (False, "Board does not exist!")
 
         # TODO delte
         self.debug_print()
@@ -144,30 +152,31 @@ class BlackBoardHost(rpyc.Service):
 
         global boards
 
-        # Check if board exists
-        if name in boards:
+        with board_lock:
+            # Check if board exists
+            if name in boards:
             
-            # Update valid state before returning
-            if boards[name]["entry_time"] + boards[name]["valid_sec"] <= time.time():
+                # Update valid state before returning
+                if boards[name]["entry_time"] + boards[name]["valid_sec"] <= time.time():
 
-                # Update valid state
-                boards[name]["is_valid"] = False
+                    # Update valid state
+                    boards[name]["is_valid"] = False
 
-                # Return the data but with invalid message
-                return (True, boards[name]["data"], boards[name]["is_valid"], "Successfully read but Data is invalid!")
+                    # Return the data but with invalid message
+                    return (True, boards[name]["data"], boards[name]["is_valid"], "Successfully read but Data is invalid!")
 
-            # Data still valid
+                # Data still valid
+                else:
+
+                    # Check for empty data
+                    if boards[name]["data"] == "":
+                        return (True, boards[name]["data"], boards[name]["is_valid"], "Successfully read but data is empty!")
+
+                    # Return Read with out problems!
+                    return (True, boards[name]["data"], boards[name]["is_valid"], "Successfully read with valid data!")
+
             else:
-
-                # Check for empty data
-                if boards[name]["data"] == "":
-                    return (True, boards[name]["data"], boards[name]["is_valid"], "Successfully read but data is empty!")
-
-                # Return Read with out problems!
-                return (True, boards[name]["data"], boards[name]["is_valid"], "Successfully read with valid data!")
-
-        else:
-            return (False, 0, False, "Blackboard does not exist!")
+                return (False, 0, False, "Blackboard does not exist!")
 
 
     def exposed_get_blackboard_status(self, name):
@@ -181,26 +190,27 @@ class BlackBoardHost(rpyc.Service):
 
         global boards
 
-        # Check if board exists
-        if name in boards:
+        with board_lock:
+            # Check if board exists
+            if name in boards:
             
-            # Check is the data is empty or not
-            if boards[name]["data"] == "":
-                is_empty = True
+                # Check is the data is empty or not
+                if boards[name]["data"] == "":
+                    is_empty = True
+                else:
+                    is_empty = False
+
+                # Update valid state before returning
+                if boards[name]["entry_time"] + boards[name]["valid_sec"] <= time.time():
+
+                    # Update valid state
+                    boards[name]["is_valid"] = False
+
+                # Return with the remaining information
+                return (True, is_empty, boards[name]["entry_time"], boards[name]["is_valid"], "Successfully read board status!")
+
             else:
-                is_empty = False
-
-            # Update valid state before returning
-            if boards[name]["entry_time"] + boards[name]["valid_sec"] <= time.time():
-
-                # Update valid state
-                boards[name]["is_valid"] = False
-
-            # Return with the remaining information
-            return (True, is_empty, boards[name]["entry_time"], boards[name]["is_valid"], "Successfully read board status!")
-
-        else:
-            return (False, False, 0, False, "Board does not exist!")
+                return (False, False, 0, False, "Board does not exist!")
 
 
 
@@ -220,8 +230,9 @@ class BlackBoardHost(rpyc.Service):
         list_of_boards = []
 
         # get all board names
-        for name in boards:
-            list_of_boards.append(name)
+        with board_lock:
+            for name in boards:
+                list_of_boards.append(name)
 
         # Check for empty list to return a different message
         if len(list_of_boards) == 0:
@@ -243,12 +254,12 @@ class BlackBoardHost(rpyc.Service):
 
         global boards
 
-        # Check if board exists
-        if name in boards:
-            del boards[name]
+        with board_lock:
+            if name in boards:
+                del boards[name]
 
-        else:
-            return (False, "Board does not exist!")
+            else:
+                return (False, "Board does not exist!")
 
         self.debug_print()
 
@@ -267,9 +278,9 @@ class BlackBoardHost(rpyc.Service):
 
         global boards
 
-        boards = {}
-
-        return (True, "Successfully deleted all boards!")
+        with board_lock:
+            boards = {}
+            return (True, "Successfully deleted all boards!")
 
         
     def write_to_log():
